ac275251eba50f76703e88b588f9b5c6c7cbe88b
IsaacThoman <isaacthoman@outlook.com>
2022-02-03 23:10:07 -0500

    working keyboard

--- public/game.js
+++ public/game.js
@@ -2,8 +2,9 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 let titleImage = new Image(); titleImage.src = 'title.png';
 
-let localPlayer = {words:['whale','monks','seams','shard','solid','water']};
+let localPlayer = {words:['','','','','','']};
 let keyCenters = [];
+let keyboardDisplayChars = 'qwertyuiop←asdfghjkl⏎zxcvbnm'.split('');
 
 
 function doFrame(){
@@ -50,25 +51,62 @@ function drawLetterBox(textColor, bgColor, x,y,width,height,char){
 
 function drawLetterBoxes(x,y,player){
     for(let yChange = 0; yChange<=225; yChange+=45)
-        for(let xChange = -90; xChange<=90; xChange+=45)
+        for(let xChange = -90; xChange<=90; xChange+=45){
+            
             drawLetterBox("#ffffff","#6aaa64",x-25+xChange,y/1.5-25+yChange,40,40,localPlayer.words[Math.floor(yChange/45)].charAt(Math.floor(xChange/45+2)));
+        }
+
 }
 
 function drawKeyboard(color,x,y,w,h){
-    let keyboardDisplayChars = 'qwertyuiopasdfghjklzxcvbnm'.split('');
     let charOn = 0;
-    for(let xChange = -145; xChange<=155; xChange+=33){
-        drawLetterBox("#ffffff","#afb0b4",x-25+xChange,y+10,30,40,keyboardDisplayChars[charOn]);
+    for(let xChange = -165; xChange<=165; xChange+=33){
+        drawLetterBox("#ffffff","#909094",x-25+xChange,y+10,30,40,keyboardDisplayChars[charOn]);
+        keyCenters[charOn] = {x:(x-25+xChange)+15,y:y+10+20}
         charOn++;
     }
     y+=45;
-    for(let xChange = -140; xChange<=140; xChange+=35) {
-        drawLetterBox("#ffffff", "#afb0b4", x - 25 + xChange, y + 10, 30, 40,keyboardDisplayChars[charOn]);
+    for(let xChange = -155; xChange<=160; xChange+=35) {
+        drawLetterBox("#ffffff", "#909094", x - 25 + xChange, y + 10, 30, 40,keyboardDisplayChars[charOn]);
+        keyCenters[charOn] = {x:(x-25+xChange)+15,y:y+10+10}
         charOn++;
     }
     y+=45;
     for(let xChange = -103; xChange<=110; xChange+=35) {
-        drawLetterBox("#ffffff", "#afb0b4", x - 25 + xChange, y + 10, 30, 40,keyboardDisplayChars[charOn]);
+        drawLetterBox("#ffffff", "#909094", x - 25 + xChange, y + 10, 30, 40,keyboardDisplayChars[charOn]);
+        keyCenters[charOn] = {x:(x-25+xChange)+15,y:y+10+10}
         charOn++;
     }
+}
+
+
+
+document.addEventListener("mousedown", mouseDownHandler, false);
+function mouseDownHandler(e) {
+    let relativeX;
+    let relativeY;
+    let rect = canvas.getBoundingClientRect();
+    if('clientX' in e) {
+        relativeX = e.clientX - rect.left;
+        relativeY = e.clientY - rect.top;
+    }
+    let keyHit = findClosestKey(relativeX,relativeY);
+    if(keyHit!=10&&keyHit!=20)
+        localPlayer.words[0] += keyboardDisplayChars[keyHit];
+    else if(keyHit==10)
+        localPlayer.words[0] = localPlayer.words[0].substring(0,localPlayer.words[0].length-1);
+
+}
+
+function findClosestKey(relativeX,relativeY){
+    let minDist = Math.sqrt(Math.pow(relativeX-keyCenters[0].x,2)+Math.pow(relativeY-keyCenters[0].y,2));
+    let minIndex = 0;
+    for(let i = 0; i<keyCenters.length; i++){
+        let thisDist = Math.sqrt(Math.pow(relativeX-keyCenters[i].x,2)+Math.pow(relativeY-keyCenters[i].y,2));
+        if(thisDist<minDist){
+            minDist = thisDist;
+            minIndex = i;
+        }
+    }
+    return minIndex;
 }
